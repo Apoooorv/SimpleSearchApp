6cab073603e34315ef23176a78dcd3acf6e57c71
Apoooorv <akhairna@asu.edu>
2018-03-03 23:36:18 -0700

    Added page not found page

--- urls.py
+++ urls.py
@@ -30,6 +30,11 @@ def register():
 		print request.form['email']
 		print request.form['password']
 		return "Hello World!"
-	
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 if __name__ == '__main__':
 	app.run(debug=False)
