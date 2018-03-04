14cac5496f8e97cd874213ea64aa4136628a3dbf
Apoooorv <akhairna@asu.edu>
2018-03-03 23:29:11 -0700

    Added registration form

--- urls.py
+++ urls.py
@@ -1,14 +1,35 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 template_dir = os.path.dirname(os.path.abspath(__file__))
 template_dir = os.path.join(template_dir, 'templates')
 
 app = Flask(__name__, template_folder=template_dir)
 
-@app.route("/")
-def hello():
-	return render_template('login.html')
+@app.route("/", methods=['GET'])
+def home():
+	return redirect(url_for('login'))
 
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	if request.method == 'GET':
+		return render_template('login.html', my_string="Hello World!")
+	else:
+		print request.form['username']
+		print request.form['password']
+		return "Hello World"
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	if request.method == 'GET':
+		return render_template('register.html')
+	else:
+		print request.form['firstname']
+		print request.form['lastname']
+		print request.form['email']
+		print request.form['password']
+		return "Hello World!"
+	
 if __name__ == '__main__':
 	app.run(debug=False)
